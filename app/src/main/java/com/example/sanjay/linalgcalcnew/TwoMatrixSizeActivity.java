56488c21338c1f84e1a94cec0e37e4b235543e8d
Sanjay <paraboosanjay@gmail.com>
2017-06-29 23:49:49 -0400

    fixed typos

--- app/src/main/java/com/example/sanjay/linalgcalcnew/TwoMatrixSizeActivity.java
+++ app/src/main/java/com/example/sanjay/linalgcalcnew/TwoMatrixSizeActivity.java
@@ -9,10 +9,10 @@ import android.view.View;
 
 /**
  * Created by Sanjay on 6/29/2017.
- * Used to get the size of both matricies. Fucntionality should be added so that this activity
- * recieves an intent message from main activity that specifies 1 or 2 matricies then this activty
- * creates sized entry based on message. Then based on that origianl message it should send that to
- * the grid view acticity so that it can tell if it needs to input another matrix. Should send size
+ * Used to get the size of both matricies. Functionality should be added so that this activity
+ * receives an intent message from main activity that specifies 1 or 2 matricies then this activity
+ * creates sized entry based on message. Then based on that original message it should send that to
+ * the grid view activity so that it can tell if it needs to input another matrix. Should send size
  * and elements in intent message. then create a result activity that displays all results for
  * matrix problems. Pass through list names activities between main menu and result should have back
  * button in tool bar that goes to matrix entry screen
