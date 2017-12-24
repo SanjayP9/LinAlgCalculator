8a80c35a928b21941ca6b0d05da3534fbced286b
Sanjay Paraboo <paraboosanjay@gmail.com>
2017-12-24 14:02:06 -0500

    Transferred RnCalcMethods to Vector classes
    this may rip

--- app/src/main/java/com/example/sanjay/linalgcalcnew/Plane.java
+++ app/src/main/java/com/example/sanjay/linalgcalcnew/Plane.java
@@ -38,6 +38,47 @@ public class Plane {
         return constant;
     }
 
+    // Distance of a point in 3D space to a plane
+    public static Fraction pointToPlane(Vector3D p, Plane plane) {
+        Vector3D p0;
+
+        if (plane.getConstant().getNumerator() == 0) {
+            p0 = new Vector3D();
+        } else {
+            p0 = new Vector3D(Fraction.divide(plane.getConstant(), plane.getX()), new Fraction(), new Fraction());
+        }
+
+        Vector3D p0p = Vector3D.subtract(p, p0);
+
+        return Vector3D.magnitude(Vector3D.projection(p0p, plane.getNormal()));
+    }
+
+    public static PlanePOIResult planeIntersection(Plane p1, Plane p2) {
+        if (Vector3D.magnitude(Vector3D.crossProduct(p1.getNormal(), p2.getNormal())).getNumerator() == 0) {
+            return new PlanePOIResult();
+        }
+
+        Vector3D lineVect = Vector3D.crossProduct(p1.getNormal(), p2.getNormal());
+
+        Fraction x = new Fraction();
+        Fraction y = new Fraction();
+        Fraction z = new Fraction();
+
+        if ((p1.getX().getNumerator() != 0) || (p2.getX().getNumerator() != 0)) {
+            x = Fraction.divide(Fraction.subtract(p2.getConstant(), p1.getConstant()), Fraction.subtract(p1.getX(), p2.getX()));
+        } else if ((p1.getY().getNumerator() != 0) || (p2.getY().getNumerator() != 0)) {
+            y = Fraction.divide(Fraction.subtract(p2.getConstant(), p1.getConstant()), Fraction.subtract(p1.getY(), p2.getY()));
+        } else if ((p1.getZ().getNumerator() != 0) || (p2.getZ().getNumerator() != 0)) {
+            y = Fraction.divide(Fraction.subtract(p2.getConstant(), p1.getConstant()), Fraction.subtract(p1.getZ(), p2.getZ()));
+        }
+
+        return new PlanePOIResult(new Vector3D(x, y, z), lineVect);
+    }
+
+    public static PlanePOIResult planeIntersection(Plane p1, Plane p2, Plane p3) {
+        return null;//temp
+    }
+
     @Override
     public String toString() {
         return this.x + " x " + this.y + " y " + this.z + " " + this.constant;
--- app/src/main/java/com/example/sanjay/linalgcalcnew/Vector2D.java
+++ app/src/main/java/com/example/sanjay/linalgcalcnew/Vector2D.java
@@ -30,6 +30,39 @@ public class Vector2D {
         this.y = y;
     }
 
+    // Adds two 2-dimensional vectors
+    public static Vector2D add(Vector2D u, Vector2D v) {
+        return new Vector2D(Fraction.add(u.getX(), v.getX()), Fraction.add(u.getY(), v.getY()));
+    }
+
+    // Subtracts two 2-dimensional vectors
+    public static Vector2D subtract(Vector2D u, Vector2D v) {
+        return add(u, scalarMultiply(v, new Fraction(-1, 1)));
+    }
+
+    // Multiplies a 2-dimensional vector by a Fraction
+    public static Vector2D scalarMultiply(Vector2D vect, Fraction scalar) {
+        return new Vector2D(Fraction.multiply(vect.getX(), scalar), Fraction.multiply(vect.getY(), scalar));
+    }
+
+    // Multiplies two 2-dimensional vectors
+    public static Vector2D multiply(Vector2D u, Vector2D v) {
+        return new Vector2D(Fraction.multiply(u.getX(), v.getX()), Fraction.multiply(u.getY(), v.getY()));
+    }
+
+    // Projection of vector u onto vector v using formula [(u⋅v)/||v||]⋅v (2D)
+    public static Vector2D projection(Vector2D u, Vector2D v) {
+        Fraction scalar = Fraction.multiply(dotProduct(u, v), (Fraction.reciprocal(dotProduct(v, v))));
+
+        return new Vector2D(Fraction.multiply(v.getX(), scalar),
+                Fraction.multiply(v.getY(), scalar));
+    }
+
+    // Dot product of two 2-dimensional vectors u1*v1 + u2*v2
+    public static Fraction dotProduct(Vector2D u, Vector2D v) {
+        return Fraction.add(Fraction.multiply(u.getX(), v.getX()), Fraction.multiply(u.getY(), v.getY()));
+    }
+
     @Override
     public String toString() {
         return x + "," + y;
